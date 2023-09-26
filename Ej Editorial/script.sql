2e8ff20e26d7fb672c624bf6513504f9ab240e84
mtnleo <mtnleonardi@gmail.com>
2023-09-26 12:51:56 -0300

    More tables, almost done

--- Ej Editorial/script.sql
+++ Ej Editorial/script.sql
@@ -28,6 +28,41 @@ CREATE TABLE libro (
 
 );
 
+CREATE TABLE volumen (
+	codigo_volumen int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    codigo_libro int NOT NULL,
+    deteriorado TINYINT(1),
+    CONSTRAINT fk_libro FOREIGN KEY (codigo_libro) REFERENCES libro (codigo_libro)
+);
+
+CREATE TABLE socio (
+	codigo_socio int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    dni int NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+    direccion VARCHAR(100),
+    localidad VARCHAR(100)
+);
+
+CREATE TABLE prestamo (
+	codigo_prestamo int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    codigo_socio int NOT NULL,
+    fecha DATE NOT NULL,
+    fecha_devolucion DATE,
+    fecha_tope DATE,
+    CONSTRAINT fk_socio FOREIGN KEY (codigo_socio) REFERENCES socio (codigo_socio)
+);
+
+CREATE TABLE prestamoXvolumen (
+	codigo_prestamoXvolumen int NOT NULL PRIMARY KEY,
+    codigo_volumen int NOT NULL,
+    codigo_prestamo int NOT NULL,
+    CONSTRAINT fk_prestamo FOREIGN KEY (codigo_prestamo) REFERENCES prestamo (codigo_prestamo),
+    CONSTRAINT fk_volumen FOREIGN KEY (codigo_volumen) REFERENCES volumen (codigo_volumen)
+
+);
+
+/* DROP PARA CONTROLAR */
 drop table libro;
 drop table autor;
 drop table editorial;
