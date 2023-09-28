8d67203362bff4f3dc47e3f74554bcc465dec793
mtnleo <mtnleonardi@gmail.com>
2023-09-28 10:59:58 -0300

    Still working on queries, now creating the Museo tables to work on them later to test the queries

--- Ej Museo/script.sql
+++ Ej Museo/script.sql
@@ -1,3 +1,64 @@
+/**********************************/
+/* CREACION DE DATABASE Y TABLAS */
+/********************************/
+
+CREATE DATABASE Museo;
+USE Museo;
+
+CREATE TABLE escuelas(
+	id_escuela int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    domicilio VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tipovisita(
+	id_tipo_visita int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    tipovisita VARCHAR(100) NOT NULL,
+    arancel int NOT NULL
+);
+
+CREATE TABLE guias(
+	id_guia int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+	apellido VARCHAR(100) NOT NULL	
+);
+
+CREATE TABLE escuelastelefonos(
+	id_escuelas_telefonos int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    telefono VARCHAR(9) NOT NULL,
+    id_escuela int NOT NULL,
+    CONSTRAINT fk_escuela FOREIGN KEY (id_escuela) REFERENCES escuelas (id_escuela)
+);
+
+CREATE TABLE reservas(
+
+
+
+);
+
+
+CREATE TABLE visitas(
+
+
+
+);
+
+
+CREATE TABLE visitasguias(
+
+
+
+);
+
+
+/* DROPS */
+DROP DATABASE museo;
+
+/********************/
+/* QUERIES SIMPLES */
+/******************/
+
+
 /* Queremos tener un listado de todos las escuelas. */
 SELECT * FROM escuelas;
 
@@ -11,7 +72,7 @@ SELECT * FROM visitas;
 SELECT * FROM reserva WHERE codigo = 2;
 
 /* ¿Existen reservas para este mes? */
-SELECT COUNT(*) FROM reserva WHERE mes = 9 AND anio = 2023;
+SELECT COUNT(*) FROM reserva WHERE MONTH(fechaReserva) = MONTH(now);
 
 /* Mostrar las escuelas cuyo código es mayor a 3 */
 SELECT * FROM escuela WHERE codigo > 3;
@@ -20,7 +81,7 @@ SELECT * FROM escuela WHERE codigo > 3;
 SELECT * FROM escuela WHERE codigo = 4;
 
 /* Mostrar las escuelas cuyo código está entre 2 y 4 */
-SELECT * FROM escuela WHERE codigo >= 2 AND codigo <= 4;
+SELECT * FROM escuela WHERE codigo BETWEEN 2 AND 4;
 
 /* Mostrar las escuelas cuyo nombre comienza con E */
 SELECT * FROM escuela WHERE nombre LIKE 'E%';
@@ -33,3 +94,30 @@ SELECT * FROM escuela WHERE codigo = 2 OR nombre LIKE '%S';
 
 /* Mostrar las escuelas cuyo codigo es mayor a 1 y su localidad sea igual a 7600 */
 SELECT * FROM escuela WHERE codigo > 1 AND localidad = 7600;
+
+/************************/
+/* QUERIES INTERMEDIAS */
+/************************/
+
+/* Listar la cantidad de Reservas realizadas para cada Escuela, ordenar el resultado por identificador de Escuela. */
+SELECT Escuelas.nombre, count(Reservas.idEscuela)
+FROM Escuelas
+JOIN Reservas ON Reservas.IdEscuela = Escuelas.IdEscuela
+GROUP BY (Escuelas.nombre)
+ORDER BY Escuelas.codigo ASC;
+
+/*
+SELECT e.nombre, count(r.id_escuelas) 
+FROM escuelas AS e
+JOIN reservas AS r
+ON e.id = r.id_escuelas
+GROUP BY (e.nombre)
+ORDER BY e.codigo ASC;
+*/
+/* Listar la cantidad de Reservas realizadas para cada Escuela, en cada mes. */
+SELECT COUNT(r.Fecha.Month), e.nombre
+FROM Escuelas as e
+JOIN Reservas as r
+ON e.id_escuela = r.id_escuela
+GROUP BY (e.nombre)
+ORDER BY (e.nombre) ASC;
