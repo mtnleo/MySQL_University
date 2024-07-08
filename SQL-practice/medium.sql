36d939d976d1c242a1e7c510eda6a517cf54de13
mtnleo <mtnleonardi@gmail.com>
2024-07-08 18:28:11 -0300

    Almost done with medium queries

--- SQL-practice/medium.sql
+++ SQL-practice/medium.sql
@@ -155,4 +155,21 @@ FROM patients
 GROUP BY first_name, last_name
 HAVING num_of_duplicate > 1
 
-/**/
+/*Display patient's full name,
+height in the units feet rounded to 1 decimal,
+weight in the unit pounds rounded to 0 decimals,
+birth_date,
+gender non abbreviated.
+
+Convert CM to feet by dividing by 30.48.
+Convert KG to pounds by multiplying by 2.205.*/
+SELECT 
+	CONCAT(first_name, ' ', last_name) AS full_name,
+    ROUND(height / 30.48, 1) AS height,
+    ROUND(weight * 2.205) AS weight,
+    birth_date,
+CASE 
+	WHEN gender = 'F' THEN 'FEMALE'
+	WHEN gender = 'M' THEN 'MALE'
+END AS 'gender'
+FROM patients
